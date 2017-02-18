ce271420e5e43aa9906e336a6942b865a6d275bc
tobihrbr <tobihrbr@gmail.com>
2017-02-18 15:28:01 +0100

    moved cli to tobihrbr/unadorned-cli

--- bin/app.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const compile = require('../lib/app.js');
-
-compile.dir(process.cwd(), process.cwd() + '/out', function (err, files) {
-  if (err) throw err;
-
-  console.log('unadorned - compiling');
-});
